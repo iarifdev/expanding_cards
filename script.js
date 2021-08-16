ed888b596794ee506313e5252aa608cd091f8acd
Muhammad Arif <talentedarif@gmail.com>
2021-08-17 00:28:34 +0500

    process_steps
    Done

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const panels = document.querySelectorAll('.panel');
 
-    panels.forEach(panel => {
-        panel.addEventListener('click', () => {
-            removeActiveClasses();
-            panel.classList.add('active')
-        })
-    })
 
-    function removeActiveClasses(){
-        panels.forEach(panel => {
-            panel.classList.remove('active')
-        })
-    }
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        remove_active()
+        panel.classList.add('active');
+    })
+})
+
+function remove_active(){
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
